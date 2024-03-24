78ab530403c43c62ac19178aec80712ff6e23a29
Oussama Berredjem <134536117+OussamaBerredjem@users.noreply.github.com>
2024-03-24 19:06:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { Telegraf,Markup} = require('telegraf')
 const axios = require('axios');
+const port = 443||process.env.PORT;
 
 
 const app = express()
@@ -10,7 +11,6 @@ try {
 const bot = new Telegraf("7178164913:AAGnQOTBmZEvK0zBPVIQkKFlQmZYsbRBeJ4")
 
 
-const port = 443||process.env.PORT;
 
 
   
